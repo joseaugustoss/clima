1f0da6901e7df9c5a47e86b3209a1c24f3b2d633
José Augusto <joseaugusto0408@gmail.com>
2024-04-06 12:16:28 -0300

    feat: limpar informações

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
   let input = document.querySelector('#searchInput').value;
   
   if(input) {
+    clearInfo();
     showWarning('Carregando ...');
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=89038ff7af518707b4778b45fae97db0&units=metric&lang=pt_br
     `;
@@ -18,20 +19,26 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
         windAngle: json.wind.deg
       });
     } else {
+      clearInfo();
       showWarning('Não encontramos está localização.');
     }
+  } else{
+    clearInfo();
   }
 });
 function showInfo(json) {
   showWarning('');
-  document.querySelector('.resultado').style.display = 'block';
   document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country} `;
   document.querySelector('.tempInfo').innerHTML =  `${json.temp} <sup>ºC</sup>`;
   document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
   document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
   document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`;
+  document.querySelector('.resultado').style.display = 'block';
 }
-
+function clearInfo(){
+  showWarning('');
+  document.querySelector('.resultado').style.display = 'none';
+}
 function showWarning(msg) {
   document.querySelector('.aviso').innerHTML = msg;
 }
